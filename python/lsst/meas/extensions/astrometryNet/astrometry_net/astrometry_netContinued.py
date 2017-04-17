ce7b5054fe71d754a32e0fc91847eeb8dd20ef70
Russell Owen <rowen@uw.edu>
2017-04-17 16:27:12 -0700

    Fix minor pep8 errors

--- python/lsst/meas/extensions/astrometryNet/astrometry_net/astrometry_netContinued.py
+++ python/lsst/meas/extensions/astrometryNet/astrometry_net/astrometry_netContinued.py
@@ -2,16 +2,15 @@ from __future__ import absolute_import
 
 from lsst.utils import continueClass
 
-from . import MultiIndex, Solver, healpixDistance, finalize
+from . import MultiIndex
 
 
-@continueClass
+@continueClass  # noqa
 class MultiIndex:
     def __iter__(self):
         """Get an iterator over the indices in a MultiIndex
-    
+
         Do not modify the number or location of the indices while using the iterator.
         """
         for i in range(len(self)):
             yield self[i]
-
